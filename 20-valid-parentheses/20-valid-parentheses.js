e07a714ce6defdde154ee8014afc0c40c5837768
Edgar Carrillo <ecarrillo046@gmail.com>
2022-06-18 17:19:15 -0700

    Time: 75 ms (70.68%), Space: 41.7 MB (92.88%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -1,95 +1,45 @@
-class Node {
-    constructor(val) {
-        this.val = val;
-        this.next = null;
-    }
-}
-
-class Stack {
-    constructor() {
-        this.first = null;
-        this.last = null;
-        this.size = 0;
-    }
-    
-    push(val) {
-        const newNode = new Node(val);
-        
-        if (!this.first) {
-            this.first = newNode;
-            this.last = newNode;
-        } else {
-            newNode.next = this.first;
-            this.first = newNode;
-        }
-        
-        this.size += 1;
-    }
-    
-    pop() {
-        if (!this.first) return undefined;
-        
-        const removedNode = this.first;
-        if (this.size === 1) {
-            this.last = null;
-        }
-        this.first = this.first.next;
-        this.size -= 1;
-        return removedNode.val;
-    }
-    
-    empty() {
-        return !this.first;
-    }
-}
-
 
 /**
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    if (s.length <= 1) return false;
-    
-    const stack = new Stack();
-    
-    for (let i = 0; i < s.length; i++) {
-        const currChar = s.charAt(i);
-        
-        switch(currChar) {
-            case '(':
-                stack.push(currChar);
-                break;
-            case '[':
-                stack.push(currChar);
-                break;
-            case '{':
-                stack.push(currChar);
-                break;
-            case '}':
-                if (stack.empty() || stack.first.val !== '{') {
-                    return false;
-                } else {
-                    stack.pop();
-                }
-                break;
-            case ')':
-                console.log(stack);
-                if (stack.empty() || stack.first.val !== '(') {
-                    return false;
-                } else {
-                    stack.pop();
-                }
-                break;
-            case ']':
-                if (stack.empty() || stack.first.val !== '[') {
-                    return false;
-                } else {
-                    stack.pop();
-                }
-                break;
-        }
+const isValid = function (s) {
+  if (s.length <= 1 || s.length % 2 !== 0) return false;
+
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    const currChar = s.charAt(i);
+
+    if (currChar === '(' || currChar === '[' || currChar === '{') {
+      stack.push(currChar);
+    } else {
+      switch (currChar) {
+        case '}':
+          if (stack.length === 0 || stack[stack.length - 1] !== '{') {
+            return false;
+          }
+          stack.pop();
+
+          break;
+        case ')':
+          if (stack.length === 0 || stack[stack.length - 1] !== '(') {
+            return false;
+          }
+          stack.pop();
+
+          break;
+        case ']':
+          if (stack.length === 0 || stack[stack.length - 1] !== '[') {
+            return false;
+          }
+          stack.pop();
+          break;
+        default:
+          break;
+      }
     }
-    
-    return !stack.first;
+  }
+
+  return stack.length === 0;
 };
