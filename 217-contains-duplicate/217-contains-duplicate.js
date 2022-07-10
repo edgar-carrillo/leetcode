dfd887a6f5982179fd44ecf78186ba271b208cea
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-10 16:59:39 -0700

    Time: 91 ms (92.20%), Space: 51.7 MB (15.49%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,14 +3,10 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const hash = {};
-    
-    for (let i = 0; i < nums.length; i++) {
-        const currNum = nums[i];
-        hash[currNum] = (hash[currNum] || 0) + 1;
-        
-        if (hash[currNum] === 2) return true;
+    let hash = {};
+    for (const num of nums) {
+        hash[num] = (hash[num] || 0) + 1;
+        if (hash[num] > 1) return true;
     }
-    
     return false;
 };
