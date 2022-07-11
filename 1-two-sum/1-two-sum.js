bc3eaf62c89773506487de63ed4ba677e8cb2bba
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-10 20:29:18 -0700

    Time: 129 ms (46.43%), Space: 42.6 MB (59.72%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -6,35 +6,26 @@
 var twoSum = function(nums, target) {
     if (!nums.length) return [];
     
-    const map = Object.create(null);
-    nums.forEach((num, i) => map[num] = i);
+    const map = {};
     
     for (let i = 0; i < nums.length; i++) {
-        const num1 = nums[i];
-        const num2 = target - num1;
-        if (map[num2] && map[num2] !== i) return [i, map[num2]];
+        const num = nums[i];
+        if (map[target - num] !== undefined) return [map[target - num], i];
+        map[num] = i;
     }
-    
-    return [];
 };
 
 /*
 
-create map with an empty object
+if the length of nums is 0, return an empty array
 
-iterate over each number in nums
-    push value into map at num
-    
-iterate over each number in nums
-    create num1 with target - current number
-    create num2 with target - num1
-    
-    if num1 and num2 are the same and are in map
-        return the value at num1 in amp
-    if num1 is in map and num2 is in map
-        return the joined value of num1 in map and num2 in map
-        
-return empty array
+declare map with an empty object
+
+iterate over each num in nums
+    if target - num is inside hash
+        return an array with target - num and i
+    otherwise
+        add num to hash with index as value
 
 */
 
