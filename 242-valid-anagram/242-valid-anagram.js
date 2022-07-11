417d74da78c771412a74f42e92864f115d80fe79
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-10 17:09:38 -0700

    Time: 99 ms (78.18%), Space: 44.5 MB (45.65%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -3,68 +3,20 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    if (s.length !== t.length) return false;
+var isAnagram = function(str1, str2) {
+    if (str1.length !== str2.length) return false;
     
-    const charHash = {};
+    const hash = Object.create(null);
     
-    for (const char of s) {
-        charHash[char] = (charHash[char] || 0) + 1;
+    for (const char of str1) {
+        hash[char] = (hash[char] || 0) + 1;
     }
     
-    for (const char of t) {
-        if (!(char in charHash)) return false;
-        
-        charHash[char] -= 1;
-        
-        if (charHash[char] < 0) return false;
+    for (const char of str2) {
+        if (!hash[char]) return false;
+        hash[char] -= 1;
+        if (hash[char] < 0) return false;
     }
     
     return true;
 };
-
-
-/* ---------- PSEUDO ---------- */
-
-/*
-
-    if both strings have different lengths
-        return false
-        
-    declare var called charHash init with empty object
-        
-    iterate over s
-         if the property of currChar does not exist in charHash
-             give it a value of 1
-         otherwise
-             increment the value by 1
-             
-    iterate over t
-        if the current character does not exist in charHash
-            return false
-        
-        decrement value at currChar by 1
-        
-        if value at currChar is negative
-            return false
-            
-    return true
-
-*/
-
-/* ---------- IOCE ---------- */
-
-/*
-
-    in:
-        - s - (string) - a word
-        - t - (string) - a word
-    out:
-        - (boolean) - true if both string inputs are anagrams of each other. false otherwise
-    cons:
-        do O(n)
-    edge:
-        - if both strings have different lengths return false
-        - case should not matter.
-
-*/
