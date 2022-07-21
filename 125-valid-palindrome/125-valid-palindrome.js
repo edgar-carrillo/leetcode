c125a1edb5334b1f7f882cdb2defdf576d791734
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-20 19:26:26 -0700

    Time: 112 ms (47.13%), Space: 47 MB (33.05%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -1,33 +1,42 @@
-const ALPHA_NUM = /^[a-zA-Z0-9]$/;
-
+function isValidChar(char) {
+    if (
+        (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z')
+        || (char >= '0' && char <= '9')
+    ) {
+        return true;
+    }
+    
+    return false;
+}
 
 /**
  * @param {string} s
  * @return {boolean}
  */
-function isPalindrome(s) {
-
+var isPalindrome = function(s) {
     let l = 0;
     let r = s.length - 1;
-
-    while(l < r){
-        while(l < r && !ALPHA_NUM.test(s[l])){
-            l++;
+    
+    while (l < r) {
+        
+        while (l < r && !isValidChar(s[l])) {
+            l += 1;
         }
-        while(l < r && !ALPHA_NUM.test(s[r])){
-            r--;
+        
+        while (l < r && !isValidChar(s[r])) {
+            r -= 1;
         }
-
-        if(s[l].toLowerCase() !== s[r].toLowerCase()) {
-            return false;
+        
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+             return false;
         }
-
-        l++;
-        r--;
+        
+        l += 1;
+        r -= 1;
     }
-
+    
     return true;
-}
+};
 
 /*
 create left with a pointer at far left side of string
