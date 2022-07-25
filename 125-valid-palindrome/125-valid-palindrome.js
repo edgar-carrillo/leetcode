80031348fac6858aa1ddf261442e74e0289c126d
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-25 09:57:13 -0700

    Time: 93 ms (72.37%), Space: 47 MB (33.92%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -1,21 +1,17 @@
 function isValidChar(char) {
-    if (
-        (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z') ||
-       (char >= '0' && char <= '9')
-    ) {
+    if (char >= '0' && char <= '9') return true;
+    if (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z') {
         return true;
     }
-    
     return false;
 }
-
 
 /**
  * @param {string} s
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    if (s.length === 1) return true;
+    if (s.length <= 1) return true;
     
     let l = 0;
     let r = s.length - 1;
@@ -29,13 +25,62 @@ var isPalindrome = function(s) {
             r -= 1;
         }
         
-        if (s[l].toLowerCase() !== s[r].toLowerCase()) {
-            return false;
-        }
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
         
         l += 1;
         r -= 1;
     }
     
-    return true
+    return true;
 };
+
+/*
+if words has 1 or less characters in it
+    return true
+
+create left with 0
+create right with the length of words - 1
+
+while left is less then right
+    while left is less then right AND and the left char is NOT valid
+        increment the value of left by 1
+        
+    while left is less then right AND the right char is NOT valid
+        decrement the value of right by 1
+        
+    if the char at left is NOT the same as the char at right
+        return false
+        
+    increment left by 1
+    decrement right by 1
+
+
+return true
+
+*/
+
+/*
+in:
+    - (string) - a string of characters
+OUT:
+    - (boolean) - true of palindrome, false otherwise
+CONS:
+    - check leetcode
+EDGE:
+    - 
+*/
+
+/*
+
+''
+RETURN: true
+
+' '
+RETURN: true
+
+'A man, a plan, a canal: Panama'
+RETURN: true
+
+'aafv'
+RETURN: false
+*/
