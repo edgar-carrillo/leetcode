72fe10ea7b877782c109866b81328d78103fe279
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-25 10:57:18 -0700

    Time: 115 ms (56.49%), Space: 42.3 MB (74.52%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,40 +4,29 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    if (!nums.length) return [];
-    
-    const map = Object.create(null);
+    const map = {};
     
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        if (map[target - num] !== undefined) return [map[target - num], i];
-        map[num] = i;
+        const currNum = nums[i];
+        if ((target - currNum) in map) {
+            return [map[target - currNum], i];
+        }
+        
+        map[currNum] = i;
     }
+    
+    return [];
 };
 
 /*
 
-if the length of nums is 0, return an empty array
+create map with an empty object
 
-declare map with an empty object
-
-iterate over each num in nums
-    if target - num is inside hash
-        return an array with target - num and i
-    otherwise
-        add num to hash with index as value
-
-*/
-
-/*
-in:
-    - nums - (array) - array of numbers
-    - target - (integer) - a number
-out:
-    - (array) - an array with the two indecies that add up to target 
-cons:
-    - only one valid answer exists.
-    - strive for O(n) or better time complexity!
-edge:
-    - none
+itereate through nums
+    if target - current number exists in map
+        return target - current numbers index and current index
+        
+    add current index as value to current number in map
+    
+return an empty array
 */
