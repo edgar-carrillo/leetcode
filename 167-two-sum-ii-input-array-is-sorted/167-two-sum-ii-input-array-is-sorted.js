44c6cb8a209a48056fda7f7561fe99dba7e94581
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-25 11:02:47 -0700

    Time: 102 ms (37.16%), Space: 43.1 MB (33.66%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -4,15 +4,30 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    let l = 0;
-    let r = numbers.length - 1;
+    let start = 0;
+    let end = numbers.length - 1;
     
-    while (l < r) {
-        const sum = numbers[l] + numbers[r];
-        if (sum > target) r -= 1;
-        else if (sum < target) l += 1;
-        else return [l + 1, r + 1];
+    while (start < end) {
+        let sum = numbers[start] + numbers[end];
+        if (sum < target) start += 1;
+        else if (sum > target) end -= 1;
+        else return [start + 1, end + 1];
     }
     
     return [];
 };
+
+/*
+create start with 0
+create end with numbers.length - 1
+
+while start is less then end
+    create sum with start and end
+    if sum is less then target increment start
+    else if sum is greater then target, decrement end
+    else return start + 1 and end + 1
+    
+return an empty array
+
+
+*/
