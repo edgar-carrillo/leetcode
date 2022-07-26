e281ed56121651d7dbd2f130995e989cb32ebc59
Edgar Carrillo <ecarrillo046@gmail.com>
2022-07-26 14:40:41 -0700

    Time: 194 ms (71.92%), Space: 52.9 MB (46.49%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -1,42 +1,31 @@
-function insertionSort(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        const currVal = nums[i];
-        for (var j = i - 1; j >= 0 & nums[j] > currVal; j--) {
-            nums[j + 1] = nums[j];
-        }
-        nums[j + 1] = currVal;
-    }
-    return nums;
-}
+
 
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    insertionSort(nums);
-    let left = 0;
-    let right;
-    const result = [];
+    nums = nums.sort((a, b) => a - b);
     
+    const result = [];
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) return result;
         if (nums[i] === nums[i - 1]) continue;
+        if (nums[i] > 0) return result;
         
-        const currNum = nums[i];
-        l = i + 1;
-        r = nums.length - 1;
-        let sum = 0;
+        const currVal = nums[i];
+        let l = i + 1;
+        let r = nums.length - 1;
         while (l < r) {
-            sum = nums[l] + nums[r] + currNum;
-            if (sum > 0) r -= 1;
-            else if (sum < 0) l += 1;
+            const sum = nums[l] + nums[r] + currVal;
+            
+            if (sum < 0) l += 1;
+            else if (sum > 0) r -= 1;
             else {
-                result.push([currNum, nums[l], nums[r]]);
+                result.push([nums[l], nums[r], currVal]);
                 l += 1;
-                r += 1;
+                r -= 1;
                 
-                while (nums[l] === nums[l - 1]) l += 1;
+                while (nums[l] === nums [l - 1]) l += 1;
                 while (nums[r] === nums[r + 1]) r -= 1;
             }
         }
@@ -45,3 +34,44 @@ var threeSum = function(nums) {
     return result;
 };
 
+/*
+sort nums array
+
+create result array
+
+iterate through nums array
+    if valeu at prev is the same as currVal
+        continue to next iteration
+        
+    if currVal is greater then 0
+        return result
+
+    create currVal with current value in nums
+    create left with currentIndex + 1
+    create right with string length - 1
+    
+    while left is less then right
+        create sum with value at left and value at right and currVal
+        
+        if sum is less then 0 
+            increment left by 1
+            
+        else if sum is greater then 0
+            decrement right by 1
+            
+        else
+            push [value at left, value at right, and currVal] into result
+            increment value of left by 1
+            decrement value of right by 1
+            
+            while the current value is the same as prev value
+                increment left by 1
+                
+            while the current valus is them same as next value
+                decrement right by 1
+            
+return result
+            
+        
+
+*/
